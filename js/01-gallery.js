71806f36ee1c9d9b74acf9f37389ab02779ab0ce
AntKhom <antonhomenko1988@gmail.com>
2023-08-18 10:48:50 -0400

    markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryListEl = document.querySelector('.gallery');
+
+
+
+
+const galleryItemsMarkup = (arr) =>
+    arr.map(
+        ({ preview, original, description }) => 
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img 
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    )
+        .join('');
+
+console.log(galleryItemsMarkup(galleryItems));
+
+galleryListEl.insertAdjacentHTML('beforeend',galleryItemsMarkup(galleryItems));
+
+
